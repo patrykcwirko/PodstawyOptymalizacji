cf8c9785b8c106ab91cf3d0647ff8e7c5b932c80
patryk <sdark431@gmail.com>
2019-05-28 15:31:23 +0200

    Wyliczanie pochodnej

--- Tool.py
+++ Tool.py
@@ -31,10 +31,34 @@ def load_file(file_path_FC='./Date_FC.txt', file_path_O='./Date_O.txt'):
     problem.ogr = ogr
     return problem
 
+def lagran(problem, x):
+    i = 0
+    k = 0
+    for list in problem.fc:
+        k = k+1
+        for j in range(len(list)):
+            if j == x and i < 3:
+                if i < 2:
+                    list[j] = list[j] * (i+1)
+                i = i+1
+            else:
+                if i < 3 and k < 3:
+                    list[j] = 0
+        if len(list) > 3:
+            if list[(x+1)%len(list)] == 0:
+                list[:] = copy.deepcopy([0] * len(list))
+            else:
+                a = list[0]
+                list[:] = copy.deepcopy([0] * len(list))
+                list[0] = copy.deepcopy(a)
+                list[x+1] = 1
+    print(problem.fc)
+
 
 
 
 
 problem = load_file()
-print(problem.ogr)
-print(problem.fc)
+lagran(problem, 0)
+# print(problem.ogr)
+# print(problem.fc)
